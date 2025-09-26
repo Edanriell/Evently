7d6d461d1143ee0a975a6aea4cce68af56cf5eb0
Edanriell <Edanriell@gmail.com>
2025-09-26 18:12:06 +0300

    feature(refactor): fixed architectural mess, and fixed some small bugs

---
+++ ModularMonolith/src/Modules/Ticketing/Evently.Modules.Ticketing.Domain/Tickets/TicketErrors.cs
@@ -0,0 +1,16 @@
+﻿using Evently.Common.Domain;
+
+namespace Evently.Modules.Ticketing.Domain.Tickets;
+
+public static class TicketErrors
+{
+	public static Error NotFound(Guid ticketId)
+	{
+		return Error.NotFound("Tickets.NotFound", $"The ticket with the identifier {ticketId} was not found");
+	}
+
+	public static Error NotFound(string code)
+	{
+		return Error.NotFound("Tickets.NotFound", $"The ticket with the code {code} was not found");
+	}
+}
